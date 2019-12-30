b27f4129abfd2894bc132eb06454b334bf8fdb4a
Suguru YAMAMOTO <sgryjp@gmail.com>
2019-12-30 23:58:00 +0900

    Scaffold the root app's layout

---
+++ src/app1/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App: React.FC = () => {
+  return (
+    <div>
+      App1
+    </div>
+  );
+};
+
+export default App;
---
+++ src/app2/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App: React.FC = () => {
+  return (
+    <div>
+      App2
+    </div>
+  );
+};
+
+export default App;
